4d5382b3161b89990676a20684c46471bcada489
AnagaD <44932290+AnagaD@users.noreply.github.com>
2026-02-01 14:57:32 -0500

    fixed double snap

--- Assets/_Scripts/Lock.cs
+++ Assets/_Scripts/Lock.cs
@@ -6,15 +6,17 @@ public class Lock : MonoBehaviour
 {
     public NPC saveme;
     public Transform lockPoint;
+    bool done = false;
 
     void OnTriggerEnter(Collider collider)
     {
-        if (collider.TryGetComponent(out Key key))
+        if (collider.TryGetComponent(out Key key) && !done)
         {
             if (key.pair == this)
             {
                 StartCoroutine(saveme.Happy());
                 key.LockTo(this);
+                done = true;
             }
             else
                 StartCoroutine(saveme.Mad());
